d553d60f45324c498df21ab8fbba3bded1b02f7b
Irtiza Babar <61447963+Irtiza90@users.noreply.github.com>
2024-08-18 16:19:22 +0500

    add APP_URL & API_URL to .env

--- re-chat-client/src/ChatInterface.js
+++ re-chat-client/src/ChatInterface.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './echo.js';
 
 const axiosI = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 0,
   headers: { 'Content-Type': 'application/json' },
 });
